5f9e18c1d6a3a1d0b35678a8fd3b9b1aef20177f
Nathan Olinto <nathanolinto@gmail.com>
2023-01-28 19:03:42 -0300

    feat: create db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { PlayerModule } from './player/player.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PlayerModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    PlayerModule,
+  ],
   controllers: [],
   providers: [],
 })
