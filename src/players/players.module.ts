cf1cf7952a498d028c992215bb9bc3191c637f63
Nathan Olinto <nathanolinto@gmail.com>
2023-01-29 20:51:28 -0300

    refactor: remove unused imports

--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -3,8 +3,6 @@ import { PlayerController } from './players.controller';
 import { PlayersService } from './players.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Player } from './entity/player.entity';
-import { Category } from 'src/categories/entities/category.entity';
-import { CategoriesModule } from 'src/categories/categories.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Player])],
