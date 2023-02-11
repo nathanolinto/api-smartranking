eab391b1d46ed65cee556b4d46c26ef0dbcf8b28
Nathan Olinto <nathanolinto@gmail.com>
2023-02-11 18:40:06 +0000

    refactor:  atualiza os imports

--- src/matches/matches.module.ts
+++ src/matches/matches.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { MatchesService } from './matches.service';
 import { MatchesController } from './matches.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { PlayersModule } from '../players/players.module';
 import { Match } from './entities/match.entity';
+import { Player } from '../players/entity/player.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Match]), PlayersModule],
+  imports: [TypeOrmModule.forFeature([Match, Player])],
   providers: [MatchesService],
   controllers: [MatchesController],
 })
