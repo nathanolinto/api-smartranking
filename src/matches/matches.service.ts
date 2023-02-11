636eaabffb12332630e4d63a592db319a6c47980
Nathan Olinto <nathanolinto@gmail.com>
2023-02-11 18:42:52 +0000

    refactor: update populate

--- src/matches/matches.service.ts
+++ src/matches/matches.service.ts
@@ -4,19 +4,20 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { populate } from '../common/typeorm/populate';
-import { PlayersService } from '../players/players.service';
 import { MongoRepository } from 'typeorm';
 import { CreateMatchDto } from './dto/create-match.dto';
 import { Match } from './entities/match.entity';
 import { ObjectId } from 'mongodb';
+import { Player } from '../players/entity/player.entity';
+import { findPopulate } from '../common/typeorm/findAndPopulate';
 
 @Injectable()
 export class MatchesService {
   constructor(
     @InjectRepository(Match)
-    private readonly matchRepository: MongoRepository<Match>,
-    private readonly playersService: PlayersService,
+    private readonly matchesRepository: MongoRepository<Match>,
+    @InjectRepository(Player)
+    private readonly playersRepository: MongoRepository<Player>,
   ) {}
 
   async createMatch(createMatchDto: CreateMatchDto): Promise<Match> {
@@ -25,54 +26,56 @@ export class MatchesService {
         `winner ${createMatchDto.winner} not includes in players ${createMatchDto.players}`,
       );
     }
-    const playerWinner = await this.playersService.getPlayerById(
-      createMatchDto.winner,
-    );
-    for (const player of createMatchDto.players) {
-      await this.playersService.getPlayerById(player);
-    }
-    const match = await this.matchRepository.save(createMatchDto);
-    const players = await populate({
-      ids: match.players,
-      service: this.playersService,
-      findById: 'getPlayerById',
+    const winnerObjectId = new ObjectId(createMatchDto.winner);
+    const playerWinner = await this.playersRepository.findBy({
+      _id: winnerObjectId,
     });
-    return Object.assign(match, { players, winner: playerWinner });
+    if (!playerWinner) {
+      throw new NotFoundException(`winner ${createMatchDto.winner} not found`);
+    }
+
+    const playersObjectId = createMatchDto.players.map(
+      (id) => new ObjectId(id),
+    );
+    for (const [index, playerObjectId] of playersObjectId.entries()) {
+      const player = await this.playersRepository.findOneBy({
+        _id: playerObjectId,
+      });
+      if (!player) {
+        throw new NotFoundException(
+          `players.${index} ${playerObjectId} not found`,
+        );
+      }
+    }
+
+    return await this.matchesRepository.save({
+      ...createMatchDto,
+      winner: winnerObjectId,
+      players: playersObjectId,
+    });
   }
 
   async getAllMatches(): Promise<Match[]> {
-    const matches = await this.matchRepository.findBy({});
-    const matchesWithPlayers = [];
-    for (const match of matches) {
-      const players = await populate({
-        ids: match.players,
-        service: this.playersService,
-        findById: 'getPlayerById',
-      });
-      const playerWinner = await this.playersService.getPlayerById(
-        match.winner,
-      );
-      matchesWithPlayers.push(
-        Object.assign(match, { players, winner: playerWinner }),
-      );
-    }
-    return matchesWithPlayers;
+    return await findPopulate({
+      repository: this.matchesRepository,
+      lookups: [
+        { from: 'player', localField: 'players' },
+        { from: 'player', localField: 'winner', isSingle: true },
+      ],
+    });
   }
 
   async getMatchById(id: string): Promise<Match> {
-    const match = await this.matchRepository.findOneBy({
-      _id: new ObjectId(id),
+    const match = await findPopulate({
+      repository: this.matchesRepository,
+      where: { _id: new ObjectId(id) },
+      lookups: [
+        { from: 'player', localField: 'players', as: 'players' },
+        { from: 'player', localField: 'winner', as: 'winner', isSingle: true },
+      ],
     });
     if (!match) {
-      throw new NotFoundException(`Match ${id} not found`);
     }
-    const players = await populate({
-      ids: match.players,
-      service: this.playersService,
-      findById: 'getPlayerById',
-    });
-    const playerWinner = await this.playersService.getPlayerById(match.winner);
-
-    return Object.assign(match, { players, winner: playerWinner });
+    return match[0];
   }
 }
